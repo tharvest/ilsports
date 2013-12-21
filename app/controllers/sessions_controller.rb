dc41139ba8162612a7b33c88c9e2d72ebf2c49b7
xutf <xu_tiefeng2004@163.com>
2013-12-22 01:35:30 +0800

    add css

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
-			redirect_to user
+			redirect_back_or user
 		else
 			flash.now[:error] = "无效的邮箱或密码"
 			render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+	before_action :signed_in_user, only: [:edit, :update]
+	before_action :correct_user, only: [:edit, :update]
+
+	def index
+		@users = User.all
+	end
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -7,14 +14,28 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def edit
+		#@user = User.find(params[:id])
+	end
+
 	def create
 		@user = User.new(user_params)
 		if @user.save
 			sign_in @user
-			flash[:success] = "欢迎那个加入，摔跤比赛！"
+			flash[:success] = "欢迎加入"
 			redirect_to @user
 		else
 			render 'new'
+		end
+	end
+
+	def update
+		#@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			flash[:success] = "Profile updated"
+			redirect_to @user
+		else
+			render 'edit'
 		end
 	end
 
@@ -26,5 +47,17 @@ class UsersController < ApplicationController
 	private
 	def user_params
 		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
+
+	def signed_in_user
+		unless signed_in_user
+			store_location
+			redirect_to signin_url, notice: "请先登录"
+		end
+	end
+
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_path) unless current_user?(@user)
 	end
 end
